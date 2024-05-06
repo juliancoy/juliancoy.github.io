98785361abfdd6c1b5b560016649db3b34369da7
Mark Reed <mreed@cd-mark.local>
2024-05-06 11:30:08 -0400

    first pass

--- script.js
+++ script.js
@@ -7,3 +7,55 @@ document.querySelectorAll('#navbar a').forEach(link => {
         });
     };
 });
+function hideTextOfOtherTiles(e){
+    console.log(e.target.dataset.tile)
+    document.querySelectorAll('.tile').forEach(tile => {
+        if(tile.dataset.tile !== e.target.dataset.tile){
+            tile.classList.add('hide-text')
+            tile.classList.remove('show-text')
+        }
+    })
+}
+function showTextOfAllTiles(){
+    document.querySelectorAll('.tile').forEach(tile => {
+        tile.classList.remove('hide-text')
+        tile.classList.add('show-text')
+    })
+}
+document.querySelectorAll('.tile').forEach(tile => {
+    tile.addEventListener('mouseenter', function(e){
+        tile.classList.add('grow')
+        tile.classList.remove('shrink')
+        hideTextOfOtherTiles(e)
+    })
+    tile.addEventListener('mouseleave', function(){
+        tile.classList.remove('grow')
+        tile.classList.add('shrink')
+        this.querySelector('.content').classList.remove('selected')
+        this.querySelectorAll('.iframe-overlay').forEach(overlay => {
+            overlay.classList.remove('hide')
+        })
+        showTextOfAllTiles()
+    })
+})
+document.querySelectorAll('.project').forEach(project => {
+    project.addEventListener('click', function(){
+        project.classList.remove('hide-project-content')
+        project.classList.add('show-project-content')
+    })
+})
+document.querySelectorAll('.close').forEach(btn => {
+    btn.addEventListener('click', function(e){
+        e.stopPropagation()
+        btn.parentElement.classList.add('hide-project-content')
+        btn.parentElement.classList.add('test')
+        btn.parentElement.classList.remove('show-project-content')
+    })
+})
+document.querySelectorAll('.talks-tile .iframe-container').forEach(iframe => {
+    iframe.addEventListener('click', function(e){
+        console.log("iframe clicked")
+        e.target.parentElement.parentElement.classList.add('selected')
+        e.target.parentElement.querySelector('.iframe-overlay').classList.add('hide')
+    })
+})
